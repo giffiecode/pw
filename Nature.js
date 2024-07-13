2a043cf95930c6a48fd63d28678b6ad44cfcddb8
giffiecode <ypei23@cmc.edu>
2024-07-13 16:04:52 -0400

    try

---
+++ Nature.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const blue_nyc_view = {
+  src: 'https://github.com/giffiecode/pw/blob/main/photography/blue2.JPG',
+  alt: 'Sky',
+  width:  '200px'
+};
+
+function Nature(){
+    return (
+      <div>
+        <h1>Blue Series</h1>
+        <img 
+          src={blue_nyc_view.src}
+          alt={blue_nyc_view.alt}
+          width={blue_nyc_view.width} />
+      </div>
+    );
+}
+
+export default Nature;
